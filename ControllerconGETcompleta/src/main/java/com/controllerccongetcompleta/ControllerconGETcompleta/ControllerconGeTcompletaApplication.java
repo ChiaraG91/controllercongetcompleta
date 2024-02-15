7a29f437049379e16eaf49c3d65e1ecea3d04e26
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-15 20:30:52 +0100

    Add files via upload

---
+++ ControllerconGETcompleta/src/main/java/com/controllerccongetcompleta/ControllerconGETcompleta/ControllerconGeTcompletaApplication.java
@@ -0,0 +1,13 @@
+package com.controllerccongetcompleta.ControllerconGETcompleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerconGeTcompletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerconGeTcompletaApplication.class, args);
+	}
+
+}
---
+++ ControllerconGETcompleta/src/test/java/com/controllerccongetcompleta/ControllerconGETcompleta/ControllerconGeTcompletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controllerccongetcompleta.ControllerconGETcompleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerconGeTcompletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
